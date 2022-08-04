9ac95a5ed26d7cf4c381729a548e3c77c4ed3eb6
Joya322 <jannatferdoush1212@gmail.com>
2022-08-04 18:07:43 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,19 @@
+// function test() {
+//     console.log("hello");
+// }
+// test();
+
+// function addNumbers(number1, number2) {
+//     var sum = number1 + number2;
+//     return sum;
+// }
+
+// var summation = addNumbers(20, 30);
+// console.log(summation);
+
+function noParameter() {
+    return 10;
+}
+
+var hasReturn = noParameter();
+console.log(hasReturn);
